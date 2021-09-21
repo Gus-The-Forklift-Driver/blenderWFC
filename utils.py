7bdf1c025dcc835886208c06943e1e53a2449acd
Gus-The-Forklift-Driver <benhubledingue@gmail.com>
2021-09-21 11:09:35 +0200

    first commit

---
+++ utils.py
@@ -0,0 +1,307 @@
+import bpy
+import math
+import sys
+from mathutils import *
+
+
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    REV = '\u001b[7m'
+    END = '\033[0m'
+
+
+def progress_bar(progress: int, max: int, step: int):
+    if progress % step == 0:
+        progress = progress / max * 100
+        width = int((progress + 1) / 4)
+        bar = "[" + "#" * width + " " * (25 - width) + "]"
+        progress = f'{round(progress,2)}%'
+
+        sys.stdout.write('\u001b[1000D')
+        sys.stdout.write(f'{bar}{progress}')
+        sys.stdout.flush()
+
+# Calculate if two points are overlapping within a given threshold.
+
+
+def is_overlapping(point1: Vector, point2: Vector, direction: str, threshold=0.001) -> bool:
+    point1 = point1.copy()
+    point2 = point2.copy()
+
+    if direction == 'x':
+        point1.x = 0
+        point2.x = 0
+    elif direction == 'y':
+        point1.y = 0
+        point2.y = 0
+    elif direction == 'z':
+        point1.z = 0
+        point2.z = 0
+    distance = (point1 - point2).length
+    if distance <= threshold:
+        # print(f'{point1.x},{point1.y},{point1.z} overlapping with {point2.x},{point2.y},{point2.z}')
+        return True
+    else:
+        return False
+
+# check if the vertices of an object can be connected to another object
+
+
+def is_mesh_compatible(object1: object, object2: object, direction: str, threshold=0.001) -> bool:
+    object1Vertices = []
+    object2Vertices = []
+
+    if direction == 'x+':
+        for verts in object1.data.vertices:
+            if verts.co.x == 1:
+                object1Vertices.append(verts.co)
+        for verts in object2.data.vertices:
+            if verts.co.x == -1:
+                object2Vertices.append(verts.co)
+        if len(object1Vertices) != len(object2Vertices):
+            return False
+        elif len(object1Vertices) == 0 and len(object2Vertices) == 0:
+            return True
+
+        overlappingVerts = 0
+        for source in object1Vertices:
+            for target in object2Vertices:
+                if is_overlapping(source, target, 'x'):
+                    overlappingVerts += 1
+        if overlappingVerts == len(object1Vertices) == len(object2Vertices):
+            return True
+        else:
+            return False
+    elif direction == 'x-':
+        for verts in object1.data.vertices:
+            if verts.co.x == -1:
+                object1Vertices.append(verts.co)
+        for verts in object2.data.vertices:
+            if verts.co.x == 1:
+                object2Vertices.append(verts.co)
+        if len(object1Vertices) != len(object2Vertices):
+            return False
+        elif len(object1Vertices) == 0 and len(object2Vertices) == 0:
+            return True
+
+        overlappingVerts = 0
+        for source in object1Vertices:
+            for target in object2Vertices:
+                if is_overlapping(source, target, 'x'):
+                    overlappingVerts += 1
+        if overlappingVerts == len(object1Vertices) == len(object2Vertices):
+            return True
+        else:
+            return False
+    elif direction == 'y+':
+        for verts in object1.data.vertices:
+            if verts.co.y == 1:
+                object1Vertices.append(verts.co)
+        for verts in object2.data.vertices:
+            if verts.co.y == -1:
+                object2Vertices.append(verts.co)
+        if len(object1Vertices) != len(object2Vertices):
+            return False
+        elif len(object1Vertices) == 0 and len(object2Vertices) == 0:
+            return True
+
+        overlappingVerts = 0
+        for source in object1Vertices:
+            for target in object2Vertices:
+                if is_overlapping(source, target, 'y'):
+                    overlappingVerts += 1
+        if overlappingVerts == len(object1Vertices) == len(object2Vertices):
+            return True
+        else:
+            return False
+    elif direction == 'y-':
+        for verts in object1.data.vertices:
+            if verts.co.y == -1:
+                object1Vertices.append(verts.co)
+        for verts in object2.data.vertices:
+            if verts.co.y == 1:
+                object2Vertices.append(verts.co)
+        if len(object1Vertices) != len(object2Vertices):
+            return False
+        elif len(object1Vertices) == 0 and len(object2Vertices) == 0:
+            return True
+
+        overlappingVerts = 0
+        for source in object1Vertices:
+            for target in object2Vertices:
+                if is_overlapping(source, target, 'y'):
+                    overlappingVerts += 1
+        if overlappingVerts == len(object1Vertices) == len(object2Vertices):
+            return True
+        else:
+            return False
+
+    elif direction == 'z+':
+        for verts in object1.data.vertices:
+            if verts.co.z == 1:
+                object1Vertices.append(verts.co)
+        for verts in object2.data.vertices:
+            if verts.co.z == -1:
+                object2Vertices.append(verts.co)
+        if len(object1Vertices) != len(object2Vertices):
+            return False
+        elif len(object1Vertices) == 0 and len(object2Vertices) == 0:
+            return True
+
+        overlappingVerts = 0
+        for source in object1Vertices:
+            for target in object2Vertices:
+                if is_overlapping(source, target, 'z'):
+                    overlappingVerts += 1
+        if overlappingVerts == len(object1Vertices) == len(object2Vertices):
+            return True
+        else:
+            return False
+    elif direction == 'z-':
+        for verts in object1.data.vertices:
+            if verts.co.z == -1:
+                object1Vertices.append(verts.co)
+        for verts in object2.data.vertices:
+            if verts.co.z == 1:
+                object2Vertices.append(verts.co)
+        if len(object1Vertices) != len(object2Vertices):
+            return False
+        elif len(object1Vertices) == 0 and len(object2Vertices) == 0:
+            return True
+
+        overlappingVerts = 0
+        for source in object1Vertices:
+            for target in object2Vertices:
+                if is_overlapping(source, target, 'z'):
+                    overlappingVerts += 1
+        if overlappingVerts == len(object1Vertices) == len(object2Vertices):
+            return True
+        else:
+            return False
+
+    else:
+        print("An error occurred during mesh compatibility checking")
+        return False
+
+
+def create_text_object(name, coordinates, text):
+    font_curve = bpy.data.curves.new(type="FONT", name="Font Curve")
+    font_curve.body = text
+    font_obj = bpy.data.objects.new(name=name, object_data=font_curve)
+    bpy.context.scene.collection.objects.link(font_obj)
+    bpy.data.objects[name].location = coordinates
+
+
+def cleanup(size):
+    done = 0
+    for x in range(size[0]):
+        for y in range(size[1]):
+            for z in range(size[2]):
+                bpy.data.objects[f'{x}.{y}.{z}.mesh'].select_set(True)
+                done += 1
+                progress_bar(done, size[0]*size[1]*size[2], 5)
+
+    objs = bpy.context.selected_objects
+    coll_target = bpy.context.scene.collection.children.get("Grid")
+    # If target found and object list not empty
+    if coll_target and objs:
+
+        # Loop through all objects
+        for ob in objs:
+            # Loop through all collections the obj is linked to
+            for coll in ob.users_collection:
+                # Unlink the object
+                coll.objects.unlink(ob)
+
+            # Link each object to the target collection
+            coll_target.objects.link(ob)
+        # bpy.data.collections["grid"].objects.link(bpy.data.objects[f'{x}.{y}.{z}.mesh'])
+
+
+def preview_grid(size: tuple, cellGrid: list):
+    done = 0
+    print('\n')
+    for x in range(size[0]):
+        for y in range(size[1]):
+            for z in range(size[2]):
+                availableTiles = cellGrid[x][y][z]
+                if len(availableTiles) == 1:
+                    bpy.ops.object.add_named(name=availableTiles[0])
+                    """
+                    bpy.ops.object.select_all(action='DESELECT')
+                    bpy.context.view_layer.objects.active = bpy.data.objects[availableTiles[0]]
+                    bpy.data.objects[availableTiles[0]].select_set(True)
+                    bpy.ops.object.duplicate(linked=True, mode='TRANSLATION')
+                    """
+                    bpy.context.active_object.name = f'{x}.{y}.{z}.mesh'
+                    bpy.data.objects[f'{x}.{y}.{z}.mesh'].location = (
+                        x*2, y*2, z*2)
+                else:
+                    pass
+                    #create_text_object(f'{x}.{y}.{z}.text', (x*2, y*2, z*2), str(len(availableTiles)))
+                done += 1
+                progress_bar(done, size[0]*size[1]*size[2], 20)
+
+
+def preview_object(location: tuple, objectName):
+    bpy.ops.object.select_all(action='DESELECT')
+    bpy.context.view_layer.objects.active = bpy.data.objects[objectName]
+    bpy.data.objects[objectName].select_set(True)
+    bpy.ops.object.duplicate(linked=True, mode='TRANSLATION')
+    bpy.context.active_object.name = f'prev_{location[0]}.{location[1]}.{location[2]}.{objectName}'
+    bpy.data.objects[f'prev_{location[0]}.{location[1]}.{location[2]}.{objectName}'].location = (
+        location[0], location[1], location[2])
+
+
+def preview_single_key(database: dict, key):
+    preview_object((0, 0, 0), key)
+    offset = 1
+    for x in database[key]['x+']:
+        preview_object((offset*2, 0, 0), x)
+        offset = offset + 1
+    offset = 1
+    for x in database[key]['x-']:
+        preview_object((offset*-2, 0, 0), x)
+        offset = offset + 1
+
+    offset = 1
+    for y in database[key]['y+']:
+        preview_object((0, offset*2, 0), y)
+        offset = offset + 1
+    offset = 1
+    for y in database[key]['y-']:
+        preview_object((0, offset*-2, 0), y)
+        offset = offset + 1
+
+    offset = 1
+    for z in database[key]['z+']:
+        preview_object((0, 0, offset*2), z)
+        offset = offset + 1
+    offset = 1
+    for z in database[key]['z-']:
+        preview_object((0, 0, offset*-2), z)
+        offset = offset + 1
+
+
+def render_step(step, size, cellGrid):
+    preview_grid(size, cellGrid)
+    bpy.context.scene.render.filepath = f'D:/Programming/017_blender/render2/img2_{step}.png'
+    bpy.context.scene.render.resolution_x = 1920
+    bpy.context.scene.render.resolution_y = 1080
+    bpy.ops.render.render(write_still=True)
+    for x in range(size[0]):
+        for y in range(size[1]):
+            for z in range(size[2]):
+                try:
+                    bpy.data.objects.remove(
+                        bpy.data.objects[f'{x}.{y}.{z}.mesh'], do_unlink=True)
+                except:
+                    pass
---
+++ wavefunction.py
@@ -0,0 +1,208 @@
+from utils import color
+import bpy
+import sys
+import random
+from mathutils import *
+sys.path.append('//utils.py')
+
+
+class waveFunction:
+    def __init__(self, database: dict, size: tuple) -> None:
+        self.database = database
+        self.size = size
+
+        self.tilesToUpdate = []
+
+        self.tileList = []
+        for keys in database:
+            self.tileList.append(str(keys))
+
+        self.cellGrid = []
+        for x in range(self.size[0]):
+            self.cellGrid.append([])
+            for y in range(self.size[1]):
+                self.cellGrid[x].append([])
+                for z in range(self.size[2]):
+                    self.cellGrid[x][y].append(self.tileList.copy())
+
+    def update_cell(self, source: tuple, target: tuple, direction: str):
+        availableSource = self.cellGrid[source[0]][source[1]][source[2]]
+        availableTarget = self.cellGrid[target[0]][target[1]][target[2]]
+        updated = False
+
+        possibleTilesList = []
+        for targ in availableTarget:
+            for possibleTile in self.database[targ][direction]:
+                if possibleTile not in possibleTilesList:
+                    possibleTilesList.append(possibleTile)
+
+        #print(f'Compairing : {color.RED}{source}{color.END}')
+        # print(availableSource)
+        #print(f'With : {color.RED}{target}{color.END}')
+        # print(possibleTilesList)
+
+        for tileSource in list(availableSource):
+            if tileSource not in possibleTilesList:
+                #print(f'{color.RED}Removing {tileSource}{color.END}')
+                availableSource.remove(tileSource)
+                updated = True
+        self.cellGrid[source[0]][source[1]][source[2]] = availableSource
+        if availableSource == []:
+            print(f'{color.REV}{color.RED} EMPTY CELL{color.END}')
+            self.cellGrid[source[0]][source[1]][source[2]] = ['air']
+        return updated
+
+    def propagate(self):
+        location = self.tilesToUpdate[0]
+        self.tilesToUpdate.pop(0)
+
+        x = location[0]
+        y = location[1]
+        z = location[2]
+
+        # print('------------------------------')
+        #print(f'{color.YELLOW}Propagating at {x},{y},{z} : {color.END}', end='')
+
+        updated = False
+
+        if len(self.cellGrid[x][y][z]) <= 1:
+            #print(f'{color.GREEN}Skipping because entropy is 1 {color.END}')
+            return
+        else:
+
+            if x != 0:
+                if self.update_cell(location, (x-1, y, z), 'x+'):
+                    updated = True
+            if x != self.size[0] - 1:
+                if self.update_cell(location, (x+1, y, z), 'x-'):
+                    updated = True
+            if y != 0:
+                if self.update_cell(location, (x, y-1, z), 'y+'):
+                    updated = True
+            if y != self.size[1] - 1:
+                if self.update_cell(location, (x, y+1, z), 'y-'):
+                    updated = True
+            if z != 0:
+                if self.update_cell(location, (x, y, z-1), 'z+'):
+                    updated = True
+            if z != self.size[2] - 1:
+                if self.update_cell(location, (x, y, z+1), 'z-'):
+                    updated = True
+
+            if updated:
+                self.update_adjacent(location)
+            # print('------------------------------')
+
+    def update_adjacent(self, location: tuple):
+        x = location[0]
+        y = location[1]
+        z = location[2]
+        if x != 0:
+            if (x-1, y, z) not in self.tilesToUpdate:
+                self.tilesToUpdate.append((x-1, y, z))
+        if x != self.size[0] - 1:
+            if (x+1, y, z) not in self.tilesToUpdate:
+                self.tilesToUpdate.append((x+1, y, z))
+        if y != 0:
+            if (x, y-1, z) not in self.tilesToUpdate:
+                self.tilesToUpdate.append((x, y-1, z))
+        if y != self.size[1] - 1:
+            if (x, y+1, z) not in self.tilesToUpdate:
+                self.tilesToUpdate.append((x, y+1, z))
+        if z != 0:
+            if (x, y, z-1) not in self.tilesToUpdate:
+                self.tilesToUpdate.append((x, y, z-1))
+        if z != self.size[2] - 1:
+            if (x, y, z+1) not in self.tilesToUpdate:
+                self.tilesToUpdate.append((x, y, z+1))
+
+    def consolidate_entropy(self):
+        maximum = len(self.tileList)
+        max = maximum
+        lowestsEntropy = []
+        for x in range(self.size[0]):
+            for y in range(self.size[1]):
+                for z in range(self.size[2]):
+                    entropy = len(self.cellGrid[x][y][z])
+                    if entropy == max:
+                        lowestsEntropy.append((x, y, z))
+                    elif entropy < max and not entropy == 1:
+                        max = entropy
+                        lowestEntropy = [(x, y, z)]
+        if maximum == max:
+            return False
+        else:
+            return random.choice(lowestEntropy)
+
+    def set_cell(self, cell: tuple, tile: str = -1):
+        #print(f'setting cell at {cell} with {tile}')
+        if tile == -1:
+            a = self.cellGrid[cell[0]][cell[1]][cell[2]]
+            #print(f'piking within {a}')
+            self.cellGrid[cell[0]][cell[1]][cell[2]] = [a.pop(
+                random.randint(0, len(a)-1))]
+        else:
+            self.cellGrid[cell[0]][cell[1]][cell[2]] = [tile]
+        return self.cellGrid[cell[0]][cell[1]][cell[2]]
+
+    def display_status(self, steps: int):
+        progress = 0.0
+        for x in range(self.size[0]):
+            for y in range(self.size[1]):
+                for z in range(self.size[2]):
+                    tilesLeft = len(
+                        self.cellGrid[x][y][z])
+                    if tilesLeft == 1:
+                        progress += 1.0
+
+        progress = progress / (self.size[0]*self.size[1]*self.size[2])
+        progress = f'{round(progress *100,2)}%'
+        sys.stdout.write('\u001b[1A')
+        sys.stdout.write('\u001b[1000D')
+        sys.stdout.write('Progress  |Cells to update|Steps\n')
+        sys.stdout.write(
+            f'{progress:<10}|{len(self.tilesToUpdate):<15}|{steps:<5}')
+        sys.stdout.flush()
+
+    def set_starting_state(self, left: str = -1, right: str = -1, front: str = -1, back: str = -1, top: str = -1, bottom: str = -1):
+
+        if bottom != -1:
+            for x in range(self.size[0]):
+                for y in range(self.size[1]):
+                    self.set_cell((x, y, 0), bottom)
+        if top != -1:
+            for x in range(self.size[0]):
+                for y in range(self.size[1]):
+                    self.set_cell((x, y, self.size[2]-1), top)
+        if left != -1:
+            for z in range(self.size[2]):
+                for y in range(self.size[1]):
+                    self.set_cell((0, y, z), left)
+        if right != -1:
+            for z in range(self.size[2]):
+                for y in range(self.size[1]):
+                    self.set_cell((self.size[0]-1, y, z), right)
+        if front != -1:
+            for z in range(self.size[2]):
+                for x in range(self.size[0]):
+                    self.set_cell((x, 0, z), front)
+        if back != -1:
+            for z in range(self.size[2]):
+                for x in range(self.size[0]):
+                    self.set_cell((x, self.size[0]-1, z), back)
+
+        pass
+
+    def set_start(self, coordinates: tuple = -1, tile: str = -1):
+        if coordinates == -1:
+            startX = random.randint(0, self.size[0]-1)
+            startY = random.randint(0, self.size[1]-1)
+            startZ = random.randint(0, self.size[2]-1)
+        else:
+            startX = coordinates[0]
+            startY = coordinates[1]
+            startZ = coordinates[2]
+
+        startingCell = self.set_cell((startX, startY, startZ), tile)
+        self.update_adjacent((startX, startY, startZ))
+        print(f'{color.GREEN}Starting at {startX}.{startY}.{startZ} with tile : {str(startingCell)}{color.END}')
